65d85f6850994ba2513088178e9c825c577119f4
Mathew Fleisch <mathew.fleisch@gmail.com>
2017-01-27 12:29:39 -0800

    Update sql file to remove comments

--- example_db_data.sql
+++ example_db_data.sql
@@ -1,25 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.14
--- http://www.phpmyadmin.net
---
--- Host: localhost:3306
--- Generation Time: Jan 27, 2017 at 12:27 PM
--- Server version: 5.6.33
--- PHP Version: 5.6.20
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `digit26_kiva`
---
-
 -- --------------------------------------------------------
 
 --
@@ -278,6 +256,3 @@ INSERT INTO `payment` (`id`, `loan_id`, `month`, `uid`, `image_id`, `name`, `amo
 (208, 1227506, 11, 'divingdoc36', 915640, 'Zaid', '3.75', '2017-11-27', 0),
 (209, 1227506, 11, 'kjartan2708', 1761762, 'Bank of Sofie', '3.75', '2017-11-27', 0);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
